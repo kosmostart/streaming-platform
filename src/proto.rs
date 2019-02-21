a3bcd55073efb3e9672eca09805826f543176a59
Maximb <sprayrules@gmail.com>
2019-02-21 13:43:18 +0000

    api

--- src/proto.rs
+++ src/proto.rs
@@ -29,7 +29,9 @@ pub struct MagicBall<T, R> where T: serde::Serialize, for<'de> T: serde::Deseria
     phantom_data_for_R: PhantomData<R>,
     addr: String,
     sender: Sender,
-    rx: crossbeam::channel::Receiver<(MsgMeta, usize, Vec<u8>)>
+    rx: crossbeam::channel::Receiver<(MsgMeta, usize, Vec<u8>)>,
+    rpc_request_rx: crossbeam::channel::Receiver<(MsgMeta, usize, Vec<u8>)>,
+    rpc_tx: crossbeam::channel::Sender<ClientMsg>
 }
 
 #[derive(Clone)]
@@ -37,6 +39,7 @@ pub struct MagicBall2 {
     addr: String,
     sender: Sender,
     rx: crossbeam::channel::Receiver<(MsgMeta, usize, Vec<u8>)>,
+    rpc_request_rx: crossbeam::channel::Receiver<(MsgMeta, usize, Vec<u8>)>,
     rpc_tx: crossbeam::channel::Sender<ClientMsg>
 }
 
@@ -44,24 +47,34 @@ pub struct MagicBall2 {
 pub struct MsgMeta {
     pub tx: String,
     pub rx: String,
+    pub kind: MsgKind,
     pub correlation_id: Option<Uuid>
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+pub enum MsgKind {
+    Event,
+    RpcRequest,
+    RpcResponse
+}
+
 impl<T, R> MagicBall<T, R> where T: Debug, T: serde::Serialize, for<'de> T: serde::Deserialize<'de>, R: Debug, R: serde::Serialize, for<'de> R: serde::Deserialize<'de> {
-    pub fn new(addr: String, sender: Sender, rx: crossbeam::channel::Receiver<(MsgMeta, usize, Vec<u8>)>) -> MagicBall<T, R> {
+    pub fn new(addr: String, sender: Sender, rx: crossbeam::channel::Receiver<(MsgMeta, usize, Vec<u8>)>, rpc_request_rx: crossbeam::channel::Receiver<(MsgMeta, usize, Vec<u8>)>, rpc_tx: crossbeam::channel::Sender<ClientMsg>) -> MagicBall<T, R> {
         MagicBall {
             phantom_data_for_T: PhantomData,
             phantom_data_for_R: PhantomData,
             addr,
             sender,
-            rx
+            rx,
+            rpc_request_rx,
+            rpc_tx
         }
     }
-    pub fn send(&self, addr: String, payload: T) -> Result<(), Error> {
-        
+    pub fn send_event(&self, addr: String, payload: T) -> Result<(), Error> {        
         let msg_meta = MsgMeta {
             tx: self.addr.clone(),
             rx: addr,
+            kind: MsgKind::Event,
             correlation_id: None
         };
 
@@ -79,33 +92,97 @@ impl<T, R> MagicBall<T, R> where T: Debug, T: serde::Serialize, for<'de> T: serd
         
         Ok(())
     }
-    pub fn recv(&self) -> Result<(MsgMeta, R), Error> {
-        let (msg_meta, len, data) = self.rx.recv()?;
+    pub fn reply_to_rpc(&self, addr: String, correlation_id: Option<Uuid>, payload: T) -> Result<(), Error> {        
+        let msg_meta = MsgMeta {
+            tx: self.addr.clone(),
+            rx: addr,
+            kind: MsgKind::RpcResponse,
+            correlation_id            
+        };
+
+        let mut msg_meta = serde_json::to_vec(&msg_meta)?;
+        let mut payload = serde_json::to_vec(&payload)?;
+
+        let mut buf = vec![];
+
+        buf.put_u32_be(msg_meta.len() as u32);
+
+        buf.append(&mut msg_meta);
+        buf.append(&mut payload);
+
+        self.sender.send(Message::Binary(buf));
         
-        //info!("{}", std::str::from_utf8(&data[len + 4..]).unwrap());
+        Ok(())
+    }
+    pub fn recv_event(&self) -> Result<(MsgMeta, R), Error> {
+        let (msg_meta, len, data) = self.rx.recv()?;            
 
-        let payload = serde_json::from_slice::<R>(&data[len + 4..])?;
-
-        info!("Deserialized payload, {:#?} {:#?}", msg_meta, payload);
+        let payload = serde_json::from_slice::<R>(&data[len + 4..])?;        
 
         Ok((msg_meta, payload))
+    }
+    pub fn recv_rpc_request(&self) -> Result<(MsgMeta, R), Error> {
+        let (msg_meta, len, data) = self.rpc_request_rx.recv()?;            
+
+        let payload = serde_json::from_slice::<R>(&data[len + 4..])?;        
+
+        Ok((msg_meta, payload))
+    }
+    pub fn rpc(&self, addr: String, mut payload: Vec<u8>) -> Result<(MsgMeta, R), Error> {
+        let correlation_id = Uuid::new_v4();
+
+        let msg_meta = MsgMeta {
+            tx: self.addr.clone(),
+            rx: addr,
+            kind: MsgKind::RpcRequest,
+            correlation_id: Some(correlation_id)
+        };
+
+        let mut msg_meta = serde_json::to_vec(&msg_meta)?;        
+
+        let mut buf = vec![];
+
+        buf.put_u32_be(msg_meta.len() as u32);
+
+        buf.append(&mut msg_meta);
+        buf.append(&mut payload);
+
+        let (rpc_tx, rpc_rx) = crossbeam::channel::unbounded();
+        
+        self.rpc_tx.send(ClientMsg::AddRpc(correlation_id, rpc_tx));
+        
+        self.sender.send(Message::Binary(buf));
+
+        let res = match self.rx.recv() {
+            Ok((msg_meta, len, data)) => {
+                let payload = &data[len + 4..];
+                let payload = serde_json::from_slice::<R>(&data[len + 4..])?;
+                Ok((msg_meta, payload))
+            }
+            Err(err) => Err(err)?
+        };
+
+        self.rpc_tx.send(ClientMsg::RemoveRpc(correlation_id));
+
+        res
     }
 }
 
 impl MagicBall2 {
-    pub fn new(addr: String, sender: Sender, rx: crossbeam::channel::Receiver<(MsgMeta, usize, Vec<u8>)>, rpc_tx: crossbeam::channel::Sender<ClientMsg>) -> MagicBall2 {
+    pub fn new(addr: String, sender: Sender, rx: crossbeam::channel::Receiver<(MsgMeta, usize, Vec<u8>)>, rpc_request_rx: crossbeam::channel::Receiver<(MsgMeta, usize, Vec<u8>)>, rpc_tx: crossbeam::channel::Sender<ClientMsg>) -> MagicBall2 {
         MagicBall2 {
             addr,
             sender,
             rx,
+            rpc_request_rx,
             rpc_tx
         }
     }
-    pub fn send(&self, addr: String, mut payload: Vec<u8>) -> Result<(), Error> {
-        
+    pub fn send_event(&self, addr: String, mut payload: Vec<u8>) -> Result<(), Error> {        
         let msg_meta = MsgMeta {
             tx: self.addr.clone(),
             rx: addr,
+            kind: MsgKind::Event,
             correlation_id: None
         };
 
@@ -122,20 +199,46 @@ impl MagicBall2 {
         
         Ok(())
     }
-    pub fn recv(&self) -> Result<(MsgMeta, Vec<u8>), Error> {
-        let (msg_meta, len, data) = self.rx.recv()?;
-                
+    pub fn reply_to_rpc(&self, addr: String, correlation_id: Option<Uuid>, mut payload: Vec<u8>) -> Result<(), Error> {        
+        let msg_meta = MsgMeta {
+            tx: self.addr.clone(),
+            rx: addr,
+            kind: MsgKind::RpcResponse,
+            correlation_id
+        };
+
+        let mut msg_meta = serde_json::to_vec(&msg_meta)?;        
+
+        let mut buf = vec![];
+
+        buf.put_u32_be(msg_meta.len() as u32);
+
+        buf.append(&mut msg_meta);
+        buf.append(&mut payload);
+
+        self.sender.send(Message::Binary(buf));
+        
+        Ok(())
+    }
+    pub fn recv_event(&self) -> Result<(MsgMeta, Vec<u8>), Error> {
+        let (msg_meta, len, data) = self.rx.recv()?;            
         let payload = &data[len + 4..];        
 
         Ok((msg_meta, payload.to_vec()))
-    }           
+    }
+    pub fn recv_rpc_request(&self) -> Result<(MsgMeta, Vec<u8>), Error> {
+        let (msg_meta, len, data) = self.rpc_request_rx.recv()?;                
+        let payload = &data[len + 4..];        
+
+        Ok((msg_meta, payload.to_vec()))
+    }
     pub fn rpc(&self, addr: String, mut payload: Vec<u8>) -> Result<(MsgMeta, Vec<u8>), Error> {
-        
         let correlation_id = Uuid::new_v4();
 
         let msg_meta = MsgMeta {
             tx: self.addr.clone(),
             rx: addr,
+            kind: MsgKind::RpcRequest,
             correlation_id: Some(correlation_id)
         };
 
--- src/simple.rs
+++ src/simple.rs
@@ -4,7 +4,7 @@ use bytes::{Buf};
 use ws::{Request, Builder, Handler, Sender, Message, Handshake, CloseCode};
 use uuid::Uuid;
 use crate::{AuthData, Config};
-use crate::proto::{ClientKind, ServerMsg, ClientMsg, MsgMeta, MagicBall, MagicBall2};
+use crate::proto::{ClientKind, ServerMsg, ClientMsg, MsgMeta, MsgKind, MagicBall, MagicBall2};
 use crate::error::Error;
 
 struct WsServer {
@@ -177,6 +177,7 @@ struct WsClient {
     addr: Option<String>,
     out: Sender,
     events_tx: crossbeam::channel::Sender<(MsgMeta, usize, Vec<u8>)>,
+    rpc_request_tx: crossbeam::channel::Sender<(MsgMeta, usize, Vec<u8>)>,
     //RequestRpc(Uuid),    
     rpc_tx: crossbeam::channel::Sender<ClientMsg>,
     //Rpc(Uuid, crossbeam::channel::Sender<(MsgMeta, usize, Vec<u8>)>)
@@ -205,34 +206,48 @@ impl Handler for WsClient {
                     Ok(msg_meta) => {
                         info!("Received message, {:#?}", msg_meta);
 
-                        match msg_meta.correlation_id {
+                        match msg_meta.kind {
+                            MsgKind::Event => {
+                                self.events_tx.send((msg_meta, len, data));
+                            }
+                            MsgKind::RpcRequest => {
+                                match msg_meta.correlation_id {
+                                    Some(correlation_id) => {
+                                        self.rpc_request_tx.send((msg_meta, len, data));
+                                    }
+                                    None => error!("Missing correlation id for RpcRequest msg {:?}", msg_meta)
+                                }                                                                
+                            }
+                            MsgKind::RpcResponse => {
+                                match msg_meta.correlation_id {
 
-                            Some(correlation_id) => {
+                                    Some(correlation_id) => {
 
-                                self.rpc_tx.send(ClientMsg::RpcDataRequest(correlation_id));
+                                        self.rpc_tx.send(ClientMsg::RpcDataRequest(correlation_id));
 
-                                match self.rpc_rx.recv() {
-                                    Ok(msg) => {
-                                        match msg {
-                                            ClientMsg::RpcDataResponse(received_correlation_id, rpc_tx) => {
-                                                match received_correlation_id == correlation_id {
-                                                    true => {
-                                                        rpc_tx.send((msg_meta, len, data));
+                                        match self.rpc_rx.recv() {
+                                            Ok(msg) => {
+                                                match msg {
+                                                    ClientMsg::RpcDataResponse(received_correlation_id, rpc_tx) => {
+                                                        match received_correlation_id == correlation_id {
+                                                            true => {
+                                                                rpc_tx.send((msg_meta, len, data));
+                                                            }
+                                                            false => error!("received_correlation_id not equals correlation_id: {}, {}", received_correlation_id, correlation_id)
+                                                        }
                                                     }
-                                                    false => error!("received_correlation_id not equals correlation_id: {}, {}", received_correlation_id, correlation_id)
+                                                    _ => error!("Client handler: wrong ClientMsg")
                                                 }
                                             }
-                                            _ => error!("Client handler: wrong ClientMsg")
+                                            Err(err) => error!("Error on self.rpc_rx.recv(): {:?}", err)
                                         }
                                     }
-                                    Err(err) => error!("Error on self.rpc_rx.recv(): {:?}", err)
+                                    None => error!("Missing correlation id for RpcResponse msg {:?}", msg_meta)
                                 }
                             }
 
-                            None => {
-                                self.events_tx.send((msg_meta, len, data));
-                            }
-                        }                                    
+                        }
+                                                            
                     }
                     Err(err) => {
                         error!("Error on message deserialization: {:?}", err);
@@ -264,33 +279,7 @@ impl Handler for WsClient {
 pub fn connect<T, R>(addr: String, host: String) -> Result<(std::thread::JoinHandle<()>, MagicBall<T, R>), Error> where T: Debug, T: Send, T: 'static, T: serde::Serialize, for<'de> T: serde::Deserialize<'de>, R: Debug, R: Send, R: 'static, R: serde::Serialize, for<'de> R: serde::Deserialize<'de> {
 
     let (events_tx, events_rx) = crossbeam::channel::unbounded();
-    let (rpc_tx, rpc_rx) = crossbeam::channel::unbounded();
-    let (tx2, rx2) = crossbeam::channel::unbounded();
-
-    let handle = std::thread::Builder::new()
-        .name(addr.clone())
-        .spawn(move || {
-            ws::connect(host, |out| {
-                tx2.send(MagicBall::new(addr.clone(), out.clone(), events_rx.clone()));
-
-                WsClient {
-                    addr: Some(addr.clone()),
-                    out,
-                    events_tx: events_tx.clone(),
-                    rpc_tx: rpc_tx.clone(),
-                    rpc_rx: rpc_rx.clone()
-                }
-            });
-        })?;
-
-    let sender = rx2.recv()?;
-
-    Ok((handle, sender))
-}
-
-pub fn connect2(addr: String, host: String) -> Result<(std::thread::JoinHandle<()>, MagicBall2), Error> {
-
-    let (events_tx, events_rx) = crossbeam::channel::unbounded();
+    let (rpc_request_tx, rpc_request_rx) = crossbeam::channel::unbounded();
     let (rpc_tx, rpc_rx) = crossbeam::channel::unbounded();
     let rpc_rx2 = rpc_rx.clone();
     let rpc_tx2 = rpc_tx.clone();
@@ -333,12 +322,77 @@ pub fn connect2(addr: String, host: String) -> Result<(std::thread::JoinHandle<(
         .name(addr.clone())
         .spawn(move || {
             ws::connect(host, |out| {
-                tx2.send(MagicBall2::new(addr.clone(), out.clone(), events_rx.clone(), rpc_tx3.clone()));
+                tx2.send(MagicBall::new(addr.clone(), out.clone(), events_rx.clone(), rpc_request_rx.clone(), rpc_tx3.clone()));
 
                 WsClient {
                     addr: Some(addr.clone()),
                     out,
                     events_tx: events_tx.clone(),
+                    rpc_request_tx: rpc_request_tx.clone(),
+                    rpc_tx: rpc_tx.clone(),
+                    rpc_rx: rpc_rx.clone()
+                }
+            });
+        })?;
+
+    let sender = rx2.recv()?;
+
+    Ok((handle, sender))
+}
+
+pub fn connect2(addr: String, host: String) -> Result<(std::thread::JoinHandle<()>, MagicBall2), Error> {
+
+    let (events_tx, events_rx) = crossbeam::channel::unbounded();
+    let (rpc_request_tx, rpc_request_rx) = crossbeam::channel::unbounded();
+    let (rpc_tx, rpc_rx) = crossbeam::channel::unbounded();
+    let rpc_rx2 = rpc_rx.clone();
+    let rpc_tx2 = rpc_tx.clone();
+    let rpc_tx3 = rpc_tx.clone();
+    let (tx2, rx2) = crossbeam::channel::unbounded();
+
+    let rpc = std::thread::Builder::new()
+        .name("rpc".to_owned())
+        .spawn(move || {
+            let mut rpcs = HashMap::new();            
+
+            //rpcs: HashMap<Uuid, crossbeam::channel::Sender<(MsgMeta, usize, Vec<u8>)>>
+
+            loop {
+                let msg = rpc_rx2.recv().unwrap();
+
+                match msg {
+                    ClientMsg::AddRpc(correlation_id, rpc_client_tx) => {
+                        info!("Adding rpc {}", correlation_id);
+                        rpcs.insert(correlation_id, rpc_client_tx);
+                    }
+                    ClientMsg::RemoveRpc(correlation_id) => {
+                        rpcs.remove(&correlation_id);
+                    }
+                    ClientMsg::RpcDataRequest(correlation_id) => {
+                        match rpcs.get(&correlation_id) {
+                            Some(rpc_client_tx) => {
+                                rpc_tx2.send(ClientMsg::RpcDataResponse(correlation_id, rpc_client_tx.clone()));
+                            }
+                            None => error!("Rpc not found: {}", correlation_id)
+                        }                        
+                    }
+                    _ => error!("Rpcs: wrong ClientMsg")
+                }
+            }
+        })
+        .unwrap();
+
+    let handle = std::thread::Builder::new()
+        .name(addr.clone())
+        .spawn(move || {
+            ws::connect(host, |out| {
+                tx2.send(MagicBall2::new(addr.clone(), out.clone(), events_rx.clone(), rpc_request_rx.clone(), rpc_tx3.clone()));
+
+                WsClient {
+                    addr: Some(addr.clone()),
+                    out,
+                    events_tx: events_tx.clone(),
+                    rpc_request_tx: rpc_request_tx.clone(),
                     rpc_tx: rpc_tx.clone(),
                     rpc_rx: rpc_rx.clone()
                 }

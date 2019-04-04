18e1bcb5384546e0aac74c7688d60f3346dec613
Maximb <sprayrules@gmail.com>
2019-04-04 07:28:04 +0000

    api

--- streaming-platform/src/lib.rs
+++ streaming-platform/src/lib.rs
@@ -7,7 +7,7 @@ mod simple {
 mod error;
 
 pub use crate::simple::{server::{start, start_with_link}, client::{connect, connect2}};
-pub use crate::proto::ClientKind;
+pub use crate::proto::{MagicBall, MagicBall2, ClientKind};
 //pub use crate::tls::{start_tls, connect_tls};
 pub use crate::error::Error;
 

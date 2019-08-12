3b368ce435347b76749e8eae41511f26420d3e4f
Maximb <sprayrules@gmail.com>
2019-08-12 08:53:03 +0000

    sp-manager

--- sp-manager/src/main.rs
+++ sp-manager/src/main.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
 use std::net::SocketAddr;
 use serde_derive::Deserialize;
 use warp::Filter;
@@ -23,46 +26,75 @@ struct Config {
 }
 
 fn main() {
-    let toml_str = r#"
-        hub_path = "C:\\src\\hubs\\target\\debug"
-        service_path = "C:\\src\\services\\target\\debug"
 
-        #hub_path = "test"
-        #service_path = "test"
+    let config_path = std::env::args().nth(1)
+        .expect("config location not passed as argument");
 
-        [server]
+    let file = File::open(config_path)
+        .expect("failed to open config");
+    //let file = File::open("ddl/".to_owned() + &service.path() + "/" + &domain.path() + ".sql")?;
+    let mut buf_reader = BufReader::new(file);
 
-        ip = "127.0.0.1"
-        port = 80
+    let mut config = String::new();
 
-        [[hubs]]
+    buf_reader.read_to_string(&mut config)
+        .expect("failed to read config");
 
-        file_name = "service-hub.exe"
-
-        [[hubs]]
-
-        file_name = "app-hub.exe"
-        
-    "#;
-
-    let config: Config = toml::from_str(toml_str).unwrap();
+    let config: Config = toml::from_str(&config).unwrap();
     println!("{:#?}", config);
 
+    let hub_path = config.hub_path
+        .expect("hub path is empty");
 
-    std::process::Command::new(config.hub_path.unwrap() + r#"/service-hub.exe"#)
-        //.arg("&")
-        .spawn()
-        .expect("ls command failed to start");
+    for hub in config.hubs.unwrap() {
 
+        match hub.file_name {
+            Some(file_name) => {
+                std::process::Command::new(hub_path.clone() + "/" + &file_name)
+                    //.arg("&")
+                    .spawn()
+                    .expect(&format!("{} command failed to start", file_name));
+            }
+            None => {
+                println!("hub with empty file name, please note");
+            }
+        }
+        
+    }
+
+    match config.services {
+        Some(services) => {
+            let service_path = config.service_path.clone()
+                .expect("service path is empty, but services are present");
+
+            for service in services {
+
+                match service.file_name {
+                    Some(file_name) => {
+                        std::process::Command::new(service_path.clone() + "/" + &file_name)
+                            //.arg("&")
+                            .spawn()
+                            .expect(&format!("{} command failed to start", file_name));
+                    }
+                    None => {
+                        println!("service with empty file name, please note");
+                    }
+                }
+            }
+
+        }
+        None => {
+            println!("no services are configured to run");
+        }
+    }
 
     let routes = warp::path("hello")
-    .and(warp::path::param())
-    .and(warp::header("user-agent"))
-    .map(|param: String, agent: String| {
-        format!("Hello {}, whose agent is {}", param, agent)
-    });
+        .and(warp::path::param())
+        .and(warp::header("user-agent"))
+        .map(|param: String, agent: String| {
+            format!("Hello {}, whose agent is {}", param, agent)
+        });
 	
     let addr = "0.0.0.0:49999".parse::<SocketAddr>().unwrap();
     warp::serve(routes).run(addr);
-        
 }

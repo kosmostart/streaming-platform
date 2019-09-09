2c09c57f65e61abb942fce714a9671078e96ea79
Maximb <sprayrules@gmail.com>
2019-09-09 12:26:36 +0000

    Command server

--- sp-manager/src/main.rs
+++ sp-manager/src/main.rs
@@ -29,14 +29,16 @@ struct StartedProcess {
     pub instance: std::process::Child
 }
 
+#[derive(Debug, Clone)]
 enum ProcessType {
     Hub,
     Service
 }
 
 enum Msg {
-    StartProcess(String),
-    StopProcess(String)
+    StartServiceProcess(String, crossbeam::channel::Sender<String>),
+    StartHubProcess(String, crossbeam::channel::Sender<String>),
+    StopProcess(String, crossbeam::channel::Sender<String>)
 }
 
 /// This is what we're going to decode into. Each field is optional, meaning
@@ -89,6 +91,7 @@ fn main() {
     let (started_tx, started_rx) = crossbeam::channel::unbounded();
 
     let start_tx = started_tx.clone();
+    let start_hub_tx = started_tx.clone();
     let stop_tx = started_tx.clone();
 
     let stated_processes = std::thread::Builder::new()
@@ -97,20 +100,20 @@ fn main() {
             let mut started = std::collections::HashMap::new();
 
             match config.hubs {
-                Some(hubs) => {
+                Some(ref hubs) => {
                     let hub_path = config.hub_path.clone()
                         .expect("hub path is empty, but hubs are present");
 
                     for hub in hubs {
 
-                        match hub.file_name {
+                        match &hub.file_name {
                             Some(file_name) => {
                                 
                                 fix_running(&running, &file_name);
 
                                 println!("starting {}", file_name);
 
-                                let instance = match hub.config {
+                                let instance = match &hub.config {
                                     Some(config) => {
                                         std::process::Command::new(hub_path.clone() + "/" + &file_name)
                                             .arg(toml::to_string(&config)
@@ -199,7 +202,20 @@ fn main() {
                 let msg = started_rx.recv().unwrap();
 
                 match msg {
-                    Msg::StartProcess(name) => {
+                    Msg::StartServiceProcess(name, reply_tx) => {
+                        system.refresh_all();
+
+                        let mut running = vec![];
+
+                        for (pid, proc_) in system.get_process_list() {
+                            //println!("{}:{} => status: {:?}", pid, proc_.name(), proc_.status());
+
+                            running.push(Process {
+                                id: *pid as usize,
+                                name: proc_.name().to_owned()
+                            });
+                        }
+
                         println!("starting {}", name);
 
                         match &config.services {
@@ -231,26 +247,88 @@ fn main() {
                                             }
                                         };
 
+                                        let process_type = ProcessType::Service;
+
                                         started.insert(name.clone(), StartedProcess {
                                             name: name.clone(),
-                                            r#type: ProcessType::Service,
+                                            r#type: process_type.clone(),
                                             instance
                                         });
 
-                                        println!("done starting {}", name);
+                                        println!("done starting {:?} {}", process_type, name);
                                     }
-                                    None => {
-                                        println!("service with empty file name, please note");
-                                    }
+                                    None => println!("service {} not found in config", name)
                                 }
                             }
-                            None => {
-                                println!("service {} not found", name);
-                            }
+                            None => println!("empty config for services")
                         }
 
+                        reply_tx.send("Ok".to_owned());
                     }
-                    Msg::StopProcess(name) => {
+                    Msg::StartHubProcess(name, reply_tx) => {
+                        system.refresh_all();
+
+                        let mut running = vec![];
+
+                        for (pid, proc_) in system.get_process_list() {
+                            //println!("{}:{} => status: {:?}", pid, proc_.name(), proc_.status());
+
+                            running.push(Process {
+                                id: *pid as usize,
+                                name: proc_.name().to_owned()
+                            });
+                        }
+
+                        println!("starting {}", name);
+
+                        match &config.hubs {
+                            Some(hubs) => {
+                                
+                                let hub_path = config.hub_path.clone()
+                                    .expect("hub path is empty, but hubs are present");
+
+                                match hubs.iter().find(|x| x.file_name == Some(name.clone())) {
+                                    Some(hub) => {
+
+                                        fix_running(&running, &name);
+
+                                        println!("starting {}", name);
+
+                                        let instance = match &hub.config {
+                                            Some(config) => {
+                                                std::process::Command::new(hub_path.clone() + "/" + &name)
+                                                    .arg(toml::to_string(&config)
+                                                        .expect("serialization to TOML string failed, check service config")
+                                                    )
+                                                    .spawn()
+                                                    .expect(&format!("{} command failed to start", name))
+                                            }
+                                            None => {
+                                                std::process::Command::new(hub_path.clone() + "/" + &name)
+                                                    .spawn()
+                                                    .expect(&format!("{} command failed to start", name))
+                                            }
+                                        };
+
+                                        let process_type = ProcessType::Hub;
+
+                                        started.insert(name.clone(), StartedProcess {
+                                            name: name.clone(),
+                                            r#type: process_type.clone(),
+                                            instance
+                                        });
+
+                                        println!("done starting {:?} {}", process_type, name);
+                                    }
+                                    None => println!("hub {} not found in config", name)
+                                }
+                            }
+                            None => println!("empty config for hubs")
+                        }
+
+                        reply_tx.send("Ok".to_owned());
+                    }
+                    Msg::StopProcess(name, reply_tx) => {
                         println!("stopping {}", name);
 
                         match started.get_mut(&name) {
@@ -263,6 +341,8 @@ fn main() {
                                 println!("{} not founded in started", name);
                             }
                         }
+
+                        reply_tx.send("Ok".to_owned());
                     }
                 }
                 
@@ -272,36 +352,51 @@ fn main() {
 
     println!("starting command server");
 
-    let routes = warp::path("hello")
-        .and(warp::header("user-agent"))
-        .map(|agent: String| {
-            format!("Hello, your agent is {}", agent)
-        })
+    let routes = 
+        warp::path("hello")
+            .and(warp::header("user-agent"))
+            .map(|agent: String| {
+                format!("Hello, your agent is {}", agent)
+            })
         .or(
             warp::path("start")
                 .and(warp::path::param())
                 .map(move |name: String| {
-                    start_tx.clone().send(Msg::StartProcess(name));
+                    let (reply_tx, reply_rx) = crossbeam::channel::unbounded();
 
-                    "operation requested successfully".to_owned()
+                    start_tx.clone().send(Msg::StartServiceProcess(name, reply_tx));
+
+                    let reply = reply_rx.recv_timeout(std::time::Duration::from_secs(30)).unwrap();
+
+                    reply
                 })
-            .or(
-                warp::path("stop")
-                    .and(warp::path::param())
-                    .map(move |name: String| {
-                        stop_tx.send(Msg::StopProcess(name));
-
-                        "operation requested successfully".to_owned()
-                    })
-            )            
         )
         .or(
-            warp::path("start")
-            .and(warp::path::param())
-            .map(|name: String| {
-                "".to_owned()
-            })
+            warp::path("start-hub")
+                .and(warp::path::param())
+                .map(move |name: String| {
+                    let (reply_tx, reply_rx) = crossbeam::channel::unbounded();
+
+                    start_hub_tx.clone().send(Msg::StartHubProcess(name, reply_tx));
+
+                    let reply = reply_rx.recv_timeout(std::time::Duration::from_secs(30)).unwrap();
+
+                    reply
+                })
         )
+        .or(
+            warp::path("stop")
+                .and(warp::path::param())
+                .map(move |name: String| {
+                    let (reply_tx, reply_rx) = crossbeam::channel::unbounded();
+
+                    stop_tx.send(Msg::StopProcess(name, reply_tx));
+
+                    let reply = reply_rx.recv_timeout(std::time::Duration::from_secs(30)).unwrap();
+
+                    reply
+                })
+        )        
         ;
 	
     let addr = "0.0.0.0:49999".parse::<SocketAddr>().unwrap();    
@@ -323,7 +418,7 @@ fn fix_running_self(running: &Vec<Process>) {
 
     for process in running {
         if process.name == name && process.id != id {
-            stop_process(process.id, &process.name);
+            stop_process_by_id(process.id, &process.name);
         }
     }
 
@@ -335,14 +430,14 @@ fn fix_running(running: &Vec<Process>, name: &str) {
 
     for process in running {
         if process.name == name {
-            stop_process(process.id, &process.name);
+            stop_process_by_id(process.id, &process.name);
         }
     }
 
     println!("done for {}", name);
 }
 
-fn stop_process(id: usize, name: &str) {
+fn stop_process_by_id(id: usize, name: &str) {
     println!("attempt to stop process {} with id {}", name, id);
 
     if cfg!(windows) {

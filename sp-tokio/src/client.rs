2917d9263bfc4e5ae91fae148e5c14e846f70cfc
Maximb <sprayrules@gmail.com>
2019-10-17 21:15:28 +0300

    sp-tokio

--- sp-tokio/src/client.rs
+++ sp-tokio/src/client.rs
@@ -6,25 +6,25 @@ use tokio::runtime::Runtime;
 use serde_json::json;
 use sp_dto::*;
 
-pub fn connect() {
+pub fn connect(addr: &str) {
     let rt = Runtime::new().expect("failed to create runtime"); 
     
-    rt.block_on(connect_future());
+    rt.block_on(connect_future(addr));
 }
 
-pub async fn connect_future() {
+pub async fn connect_future(addr: &str) {
     let mut stream = TcpStream::connect("127.0.0.1:60000").await.unwrap();
     let (mut socket_read, mut socket_write) = stream.split();
 
     //let mut data_buf = [0; 1024];
 
     let route = Route {
-        source: Participator::Service("qwe".to_owned()),
+        source: Participator::Service(addr.to_owned()),
         spec: RouteSpec::Simple,
-        points: vec![Participator::Service("qwe".to_owned())]
+        points: vec![Participator::Service(addr.to_owned())]
     };  
 
-    let rpc_dto = rpc_dto("asd".to_owned(), "qwe".to_owned(), "key".to_owned(), json!({
+    let rpc_dto = rpc_dto(addr.to_owned(), addr.to_owned(), "key".to_owned(), json!({
     }), route).unwrap();
 
     if let Err(err) = socket_write.write_all(&rpc_dto).await {
@@ -42,6 +42,8 @@ pub async fn connect_future() {
                 return;
             }
         };
+
+        println!("client n {}", n);
     }
 
     //Ok(())

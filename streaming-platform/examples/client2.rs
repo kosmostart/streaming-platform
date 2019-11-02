212407a8b047be5cacd522024320283e139e105d
Maximb <sprayrules@gmail.com>
2019-11-02 20:58:05 +0000

    Simple mode

--- streaming-platform/examples/client2.rs
+++ streaming-platform/examples/client2.rs
@@ -1,10 +1,18 @@
-use streaming_platform::{magic_ball, Mode};
+use serde_json::{Value, json};
+use streaming_platform::{magic_ball, Mode, MagicBall, sp_dto::MsgMeta};
 
 fn main() {
     let host = "127.0.0.1:60000";
     let addr = "SuperService2";
     let access_key = "";
-    let mode = Mode::FullMessage;
+    let mode = Mode::FullMessageSimple(process_event, process_rpc_request);
 
     magic_ball(host, addr, access_key, mode);
+}
+
+fn process_event(mb: &mut MagicBall, msg_meta: &MsgMeta, payload: Value, attachments: Vec<u8>) {
+
+}
+fn process_rpc_request(mb: &mut MagicBall, msg_meta: &MsgMeta, payload: Value, attachments: Vec<u8>) -> Value {
+    json!({})
 }

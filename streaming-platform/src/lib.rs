bafd8db75de60de55d5b7ed8fb390b0757060e6b
Maximb <sprayrules@gmail.com>
2020-01-12 01:41:59 +0000

    message split feature

--- streaming-platform/src/lib.rs
+++ streaming-platform/src/lib.rs
@@ -1,13 +1,12 @@
 #![recursion_limit="512"]
 #![feature(try_trait)]
-/*
 pub use futures;
 pub use tokio;
 pub use sp_dto;
 pub use proto::{ServerConfig, ClientMsg, ProcessStream, ProcessEventRaw, ProcessRpcRaw, ProcessEvent, ProcessRpc, StreamStartup, Startup, MagicBall, ProcessError, RestreamMsg};
 pub use server::{start, start_future};
 pub use client::{stream_mode, full_message_raw_mode, full_message_mode};
-*/
+
 mod proto;
 pub mod server;
 pub mod client;

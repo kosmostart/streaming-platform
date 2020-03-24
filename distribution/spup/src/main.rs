4803617307d5081a67f6aaa41da678baa215239e
Maximb <sprayrules@gmail.com>
2020-03-24 21:20:24 +0000

    spup

--- distribution/spup/src/main.rs
+++ distribution/spup/src/main.rs
@@ -8,9 +8,7 @@ mod cfg;
 
 struct FileStreamLayout {
     stream: StreamLayout,
-    payload: Option<Value>,
-    download_payload: Option<Value>,
-    file_id: Option<Uuid>,
+    payload: Option<Value>,    
     file: Option<File>,
     rpc_result: RpcResult
 }
@@ -29,7 +27,7 @@ pub async fn startup(config: HashMap<String, String>, mut mb: MagicBall) {
     let access_key = config.get("access_key").expect("access key is empty");
     let (correlation_id, dto, msg_meta_size, payload_size, attachments_sizes) = rpc_dto_with_correlation_id_sizes(
         mb.addr.clone(),
-        "File".to_owned(),
+        "SpFileSvc".to_owned(),
         "Download".to_owned(),
         json!({
             "access_key": access_key
@@ -50,13 +48,12 @@ pub async fn startup(config: HashMap<String, String>, mut mb: MagicBall) {
     ).await.expect("failed to write download rpc dto");
 }
 
-pub async fn process_stream(config: HashMap<String, String>, mut mb: MagicBall, mut rx: Receiver<ClientMsg>, _: Option<Receiver<RestreamMsg>>) {
-    let storage_path = config.get("storage_path").expect("missing storage_path config value");
+pub async fn process_stream(config: HashMap<String, String>, mut mb: MagicBall, mut rx: Receiver<ClientMsg>, _: Option<Receiver<RestreamMsg>>) {    
     let mut stream_layouts = HashMap::new();    
     loop {        
         let client_msg = rx.recv().await.expect("connection issues acquired");
         let stream_id = client_msg.get_stream_id();
-        match process_client_msg(&mut mb, &mut stream_layouts, storage_path, client_msg).await {
+        match process_client_msg(&mut mb, &mut stream_layouts, client_msg).await {
             Ok(()) => {}
             Err(e) => {
                 match stream_id {
@@ -93,7 +90,7 @@ pub async fn process_stream(config: HashMap<String, String>, mut mb: MagicBall, 
     }
 }
 
-async fn process_client_msg(mb: &mut MagicBall, stream_layouts: &mut HashMap<u64, FileStreamLayout>, storage_path: &str, client_msg: ClientMsg) -> Result<(), Error> {
+async fn process_client_msg(mb: &mut MagicBall, stream_layouts: &mut HashMap<u64, FileStreamLayout>, client_msg: ClientMsg) -> Result<(), Error> {
     match client_msg {
         ClientMsg::MsgMeta(stream_id, msg_meta) => {            
             stream_layouts.insert(stream_id, FileStreamLayout {
@@ -103,9 +100,7 @@ async fn process_client_msg(mb: &mut MagicBall, stream_layouts: &mut HashMap<u64
                     payload: vec![],
                     attachments_data: vec![]
                 },
-                payload: None,
-                download_payload: None,
-                file_id: None,
+                payload: None,                
                 file: None,
                 rpc_result: RpcResult::Ok
             });
@@ -118,24 +113,20 @@ async fn process_client_msg(mb: &mut MagicBall, stream_layouts: &mut HashMap<u64
             let stream_layout = stream_layouts.get_mut(&stream_id).ok_or(Error::CustomError("not found stream for payload finish".to_owned()))?;
             stream_layout.stream.payload.extend_from_slice(&buf[..n]);
             match stream_layout.stream.msg_meta.key.as_ref() {
-                "Upload" => {
+                "Download" => {
                     let attachment = stream_layout.stream.msg_meta.attachments.iter().nth(0).ok_or(Error::CustomError("no attachment found in msg meta for upload key".to_owned()))?;
                     let payload: Value = from_slice(&stream_layout.stream.payload)?;                    
                     let mut path = String::new();                    
                     stream_layout.file = Some(File::create(path).await?);
                     stream_layout.payload = Some(payload);
-                }
-                "Download" => {
-                    let payload: Value = from_slice(&stream_layout.stream.payload)?;
-                    stream_layout.download_payload = Some(payload);                        
-                }
+                }                
                 _ => {}
             }                        
         }
         ClientMsg::AttachmentData(stream_id, index, n, buf) => {
             let stream_layout = stream_layouts.get_mut(&stream_id).ok_or(Error::CustomError("not found stream for attachment data".to_owned()))?;
             match stream_layout.stream.msg_meta.key.as_ref() {
-                "Upload" => {
+                "Dowload" => {
                     let file = stream_layout.file.as_mut().ok_or(Error::CustomError("file is empty for attachment data".to_owned()))?;
                     file.write_all(&buf[..n]).await?;
                 }
@@ -145,7 +136,7 @@ async fn process_client_msg(mb: &mut MagicBall, stream_layouts: &mut HashMap<u64
         ClientMsg::AttachmentFinished(stream_id, index, n, buf) => {
             let stream_layout = stream_layouts.get_mut(&stream_id).ok_or(Error::CustomError("not found stream for attachment finish".to_owned()))?;
             match stream_layout.stream.msg_meta.key.as_ref() {
-                "Upload" => {
+                "Download" => {
                     let file = stream_layout.file.as_mut().ok_or(Error::CustomError("file is empty for attachment data".to_owned()))?;
                     file.write_all(&buf[..n]).await?;
                     stream_layout.file = None;
@@ -154,67 +145,7 @@ async fn process_client_msg(mb: &mut MagicBall, stream_layouts: &mut HashMap<u64
             }                                        
         }
         ClientMsg::MessageFinished(stream_id) => {                                
-            let stream_layout = stream_layouts.remove(&stream_id).ok_or(Error::CustomError("not found stream for message finish".to_owned()))?;
-            match stream_layout.stream.msg_meta.key.as_ref() {                            
-                "Upload" => {                    
-                    let file_id = stream_layout.file_id.ok_or(Error::CustomError("empty file id for upload message finish".to_owned()))?;
-                    let payload = stream_layout.payload.ok_or(Error::CustomError("empty payload for upload message finish".to_owned()))?;                                        
-                    let reponse_payload = to_vec(&json!({
-
-                    }))?;
-                    let mut route = stream_layout.stream.msg_meta.route.clone();
-                    route.points.push(Participator::Service(mb.addr.clone()));
-                    let (res, msg_meta_size, payload_size, attachments_size) = reply_to_rpc_dto2_sizes(
-                        mb.addr.clone(), 
-                        stream_layout.stream.msg_meta.tx.clone(), 
-                        stream_layout.stream.msg_meta.key.clone(), 
-                        stream_layout.stream.msg_meta.correlation_id, 
-                        reponse_payload,
-                        vec![], vec![],
-                        stream_layout.rpc_result.clone(),
-                        route
-                    )?;
-                    mb.write_vec(stream_layout.stream.id, res, msg_meta_size, payload_size, attachments_size).await.expect("failed to write response to upload");
-                }
-                "Download" => {
-                    let FileStreamLayout { 
-                        stream,
-                        payload: _,
-                        download_payload,
-                        file_id: _,
-                        file: _,
-                        rpc_result: _
-                    } = stream_layout;
-                    let payload = download_payload.ok_or(Error::CustomError("empty download payload for download message finish".to_owned()))?;
-                    let service_path = payload["service_path"].as_str().ok_or(Error::OptionIsNone)?;
-                    let domain_path = payload["domain_path"].as_str().ok_or(Error::OptionIsNone)?;
-                    let token_path = payload["token_path"].as_str().ok_or(Error::OptionIsNone)?;
-                    let file_id = payload["file_id"].as_str().ok_or(Error::OptionIsNone)?;
-                    let file_name = payload["file_name"].as_str().ok_or(Error::OptionIsNone)?.to_owned();
-                    let mut path = String::new();
-                    path.push_str(&storage_path);
-                    path.push_str("/");
-                    path.push_str(service_path);
-                    path.push_str("/");
-                    path.push_str(domain_path);
-                    path.push_str("/");
-                    path.push_str(token_path);
-                    path.push_str("/");
-                    path.push_str(file_id);
-                    let mb = mb.clone();
-                    tokio::spawn(async move {                    
-                        match download_file(mb, stream.msg_meta, path, file_name.clone()).await {
-                            Ok(()) => {
-                                info!("file download complete, name {}", file_name);
-                            }
-                            Err(e) => {
-                                error!("download file error {:?}", e);
-                            }
-                        }
-                    });                    
-                }
-                _ => {}
-            }
+            let stream_layout = stream_layouts.remove(&stream_id).ok_or(Error::CustomError("not found stream for message finish".to_owned()))?;            
         }
         _ => {}
     }

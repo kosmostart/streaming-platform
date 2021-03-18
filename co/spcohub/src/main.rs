b333da3acafa99eaed746145c788ab5da87ca841
Maximb <sprayrules@gmail.com>
2021-03-18 11:34:50 +0300

    co

--- co/spcohub/src/main.rs
+++ co/spcohub/src/main.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-use streaming_platform::{sp_cfg, server::start, sp_dto::Subscribes};
+use streaming_platform::{sp_cfg, server::start, sp_dto::{Key, Subscribes}};
 
 fn main() {
     env_logger::init();
@@ -7,8 +7,24 @@ fn main() {
     let config = sp_cfg::get_config_from_arg();
 
     let event_subscribes = HashMap::new();
-    let rpc_subscribes = HashMap::new();
-    let rpc_response_subscribes = HashMap::new();
+    let mut rpc_subscribes = HashMap::new();
+    let mut rpc_response_subscribes = HashMap::new();
 
-    start(config, Subscribes::ByKey(event_subscribes, rpc_subscribes, rpc_response_subscribes));
+    rpc_subscribes.insert("Build".to_owned(), vec![
+        Key::new("Deploy", "", "")
+    ]);
+
+    rpc_subscribes.insert("Pod".to_owned(), vec![
+        Key::new("DeployPack", "", "")
+    ]);
+
+    rpc_response_subscribes.insert("Build".to_owned(), vec![
+        Key::new("DeployPack", "", "")
+    ]);
+
+    rpc_response_subscribes.insert("Web".to_owned(), vec![
+        Key::new("Deploy", "", "")
+    ]);
+
+    start(config, Subscribes::ByAddr(event_subscribes, rpc_subscribes, rpc_response_subscribes));
 }

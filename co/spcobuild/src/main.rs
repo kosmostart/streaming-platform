a6c22fe843045be501f01dfe4c4c09cbd9a89a57
Maximb <sprayrules@gmail.com>
2021-03-19 15:59:27 +0300

    co

--- co/spcobuild/src/main.rs
+++ co/spcobuild/src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::io::Read;
 use serde_json::{json, Value, from_value};
 use log::*;
 use streaming_platform::{client, MagicBall, sp_dto::{MsgMeta, Message, Response, resp}};
@@ -11,6 +12,20 @@ mod repository {
     pub mod commit;
 }
 
+#[derive(Debug)]
+enum SideKick {
+    IncorrectKeyInRequest,
+    //SerdeJson(serde_json::Error)
+}
+
+impl std::fmt::Display for SideKick {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "SuperErrorSideKick is here!")
+    }
+}
+
+impl std::error::Error for SideKick {}
+
 pub async fn process_event(config: HashMap<String, String>, mut mb: MagicBall, msg: Message<Value>, _: ()) -> Result<(), Box<dyn std::error::Error>>  {
     //info!("{:#?}", msg);
     
@@ -20,9 +35,53 @@ pub async fn process_event(config: HashMap<String, String>, mut mb: MagicBall, m
 pub async fn process_rpc(config: HashMap<String, String>, mut mb: MagicBall, msg: Message<Value>, _: ()) -> Result<Response<Value>, Box<dyn std::error::Error>> {   
     info!("{:#?}", msg);
 
-    resp(json!({
-        "data": "hi"
-    }))
+    let res = match msg.meta.key.action.as_ref() {
+        "Deploy" => {
+            let path = "d:/src/streaming-platform/Cargo.toml";
+
+            let cmd = "cargo";
+            
+            let args = [
+                "build",
+                "--release",
+                "--manifest-path",
+                path
+            ];
+
+            let args = ["--help"];
+
+            let mut handle = std::process::Command::new(cmd)
+                .args(&args)
+                //.stdin(std::process::Stdio::piped())
+                .stdout(std::process::Stdio::piped())
+                //.stderr(std::process::Stdio::piped())
+                .spawn()
+                .expect("");
+
+            let mut stdout = handle.stdout.take().unwrap();
+
+            println!("2");
+        
+            let mut buffer = [0; 100];
+        
+            // read up to 10 bytes
+            stdout.read(&mut buffer).unwrap();
+        
+            println!("{:?}", buffer);
+        
+            let ecode = handle.wait().expect("failed to wait on child");
+        
+            println!("{:?}", ecode);
+
+            //pack();
+
+            json!({
+            })
+        }
+        _ => return Err(Box::new(SideKick::IncorrectKeyInRequest))
+    };
+
+    resp(res)
 }
 
 pub async fn startup(config: HashMap<String, String>, mut mb: MagicBall, startup_data: Option<Value>, _: ()) {

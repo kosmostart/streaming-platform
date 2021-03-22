b33ad93c671c9f425a22cd0660787c8bd93bedda
Maximb <sprayrules@gmail.com>
2021-03-22 22:51:44 +0300

    stream

--- sp-web/src/lib.rs
+++ sp-web/src/lib.rs
@@ -64,6 +64,16 @@ pub async fn process_rpc(_config: HashMap<String, String>, mut _mb: MagicBall, _
 }
 
 pub async fn startup(config: HashMap<String, String>, mb: MagicBall, startup_data: Option<Value>, _: ()) {
+	/*
+	let (mut restream_tx, mut restream_rx) = mpsc::unbounded_channel();    
+    let config2 = config.clone();
+    tokio::spawn(async move {
+        let stream_addr = config2.get("stream_addr").expect("missing stream_addr config value").to_owned();
+        let host = config2.get("host").expect("missing host config value").to_owned();
+        let access_key = "";
+        stream_mode(&host, &stream_addr, access_key, process_stream, async move |_, _| {}, config2, Some(restream_rx)).await;
+    });
+	*/
     let listen_addr = config.get("listen_addr").expect("Missing listen_addr config value");
     let cert_path = config.get("cert_path");
     let key_path = config.get("key_path");
@@ -339,3 +349,221 @@ pub fn response_with_cookie(aca_origin: String, cookie_header: &str, data: Vec<u
         .header(SET_COOKIE, cookie_header)
         .body(data) 
 }
+
+/*
+struct DownloadStreamLayout {
+    stream: StreamLayout,
+    body_tx: Option<hyper::body::Sender>,
+    completion_tx: Option<oneshot::Sender<StreamCompletion>>
+}
+
+pub enum InnerMsg {
+    AddRestream(String, hyper::body::Sender, Option<oneshot::Sender<StreamCompletion>>),
+    GetRestream(String, oneshot::Sender<(hyper::body::Sender, Option<oneshot::Sender<StreamCompletion>>)>)
+}
+
+pub async fn process_stream(config: HashMap<String, String>, mut mb: MagicBall, mut rx: UnboundedReceiver<ClientMsg>, mut restream_rx: Option<UnboundedReceiver<RestreamMsg>>) {    
+    let mut restream_rx = restream_rx.expect("restream rx is empty");
+    let (mut inner_tx, mut inner_rx) = mpsc::unbounded_channel();
+    let mut inner_tx2 = inner_tx.clone();
+    let mut mb2 = mb.clone();
+    tokio::spawn(async move {
+        let mut restreams = HashMap::new();
+        loop {
+            match inner_rx.recv().await.expect("restream channel dropped") {
+                InnerMsg::AddRestream(correlation_id, body_tx, completion_tx) => {
+                    restreams.insert(correlation_id, (body_tx, completion_tx));
+                }                
+                InnerMsg::GetRestream(correlation_id, reply) => {
+                    let restream = restreams.remove(&correlation_id).expect("restream not found for get");
+                    match reply.send(restream) {
+                        Ok(()) => {}
+                        Err(_) => panic!("send restream failed")
+                    }
+                }
+            }
+        }
+    });    
+    tokio::spawn(async move {
+        loop {
+            match restream_rx.recv().await.expect("restream channel dropped") {
+                RestreamMsg::StartHttp(payload, body_tx, completion_tx) => {
+                    let (correlation_id, dto, msg_meta_size, payload_size, attachments_sizes) = rpc_dto_with_correlation_id_sizes(
+                        mb.addr.clone(),
+                        "File".to_owned(), 
+                        "Download".to_owned(), 
+                        payload, 
+                        Route {
+                            source: Participator::Service(mb.addr.clone()),
+                            spec: RouteSpec::Simple,
+                            points: vec![Participator::Service(mb.addr.clone())]
+                        },
+                        None,
+                        None
+                    ).expect("failed to create download rpc dto");
+                    match inner_tx2.send(InnerMsg::AddRestream(correlation_id.to_string(), body_tx, completion_tx)) {
+                        Ok(()) => {}
+                        Err(_) => panic!("InnerMsg::Addrestream send error")
+                    }
+                    let stream_id = mb.get_stream_id();
+                    mb.write_vec(
+                        stream_id,
+                        dto, 
+                        msg_meta_size, 
+                        payload_size, 
+                        attachments_sizes
+                    ).await.expect("failed to write download rpc dto");
+                }
+                _ => error!("incorrect restream msg")
+            }
+        }
+    });
+    let mut stream_layouts = HashMap::new();
+    loop {        
+        let client_msg = rx.recv().await.expect("connection issues acquired");
+        let stream_id = client_msg.get_stream_id();
+        match process_client_msg(&mut mb2, &mut stream_layouts, client_msg, &mut inner_tx).await {
+            Ok(()) => {}
+            Err(e) => {
+                match stream_id {
+                    Some(stream_id) => {
+                        match stream_layouts.remove(&stream_id) {
+                            Some(stream_layout) => {
+                                match stream_layout.stream.msg_meta.kind {
+                                    MsgKind::RpcRequest => {
+                                        let mut route = stream_layout.stream.msg_meta.route.clone();
+                                        route.points.push(Participator::Service(mb2.addr.clone()));
+                                        let (res, msg_meta_size, payload_size, attachments_size) = reply_to_rpc_dto2_sizes(
+                                            mb2.addr.clone(), 
+                                            stream_layout.stream.msg_meta.tx.clone(), 
+                                            stream_layout.stream.msg_meta.key.clone(), 
+                                            stream_layout.stream.msg_meta.correlation_id, 
+                                            vec![],
+                                            vec![], vec![],
+                                            RpcResult::Err,
+                                            route,
+                                            None,
+                                            None
+                                        ).expect("failed to create rpc reply");
+                                        mb2.write_vec(stream_layout.stream.id, res, msg_meta_size, payload_size, attachments_size).await.expect("failed to write response to upload");
+                                    }
+                                    _ => {}
+                                }                        
+                            }
+                            None => {}
+                        }
+                    }
+                    None => {
+
+                    }
+                }                
+                error!("{:?}", e);
+            }
+        }
+    }
+}
+
+async fn process_client_msg(mb: &mut MagicBall, stream_layouts: &mut HashMap<u64, DownloadStreamLayout>, client_msg: ClientMsg, inner_tx: &mut UnboundedSender<InnerMsg>) -> Result<(), Error> {
+    match client_msg {
+        ClientMsg::MsgMeta(stream_id, msg_meta) => {
+            let (get_restream_tx, get_restream_rx) = oneshot::channel();
+            match inner_tx.send(InnerMsg::GetRestream(msg_meta.correlation_id.to_string(), get_restream_tx)) {
+                Ok(()) => {}
+                Err(_) => panic!("InnerMsg::GetRestream send error")
+            }
+            let (body_tx, completion_tx) = get_restream_rx.await.expect("failed to get restream");                                
+            stream_layouts.insert(stream_id, DownloadStreamLayout {
+                stream: StreamLayout {
+                    id: stream_id,
+                    msg_meta,
+                    payload: vec![],
+                    attachments_data: vec![]
+                },
+                body_tx: Some(body_tx),
+                completion_tx
+            });                
+        }
+        /*
+        ClientMsg::PayloadData(stream_id, n, buf) => {                                
+            match stream_layouts.get_mut(&stream_id) {
+                Some(stream_layout) => {
+                    stream_layout.stream.payload.extend_from_slice(&buf[..n]);
+                }
+                None => error!("not found stream {} for payload data", stream_id)
+            }                
+        }
+        ClientMsg::PayloadFinished(stream_id, n, buf) => {
+            match stream_layouts.get_mut(&stream_id) {
+                Some(stream_layout) => {
+                    stream_layout.stream.payload.extend_from_slice(&buf[..n]);
+                }
+                None => error!("not found stream {} for payload data", stream_id)
+            }
+        }
+        */
+        ClientMsg::AttachmentData(stream_id, index, n, buf) => {                
+            let stream_layout = stream_layouts.get_mut(&stream_id).ok_or(Error::CustomError("not found stream for attachment data".to_owned()))?;
+            let body_tx = stream_layout.body_tx.as_mut().ok_or(Error::CustomError("body tx is empty for attachment data".to_owned()))?;
+            let unit = Bytes::copy_from_slice(&buf[..n]);
+            match body_tx.send_data(unit).await {
+                Ok(()) => {}
+                Err(e) => return Err(Error::CustomError(format!("{:?}", e)))
+            }  
+        }
+        ClientMsg::AttachmentFinished(stream_id, index, n, buf) => {
+            let stream_layout = stream_layouts.get_mut(&stream_id).ok_or(Error::CustomError("not found stream for attachment finish".to_owned()))?;
+            let body_tx = stream_layout.body_tx.as_mut().ok_or(Error::CustomError("body tx is empty for attachment finish".to_owned()))?;
+            let unit = Bytes::copy_from_slice(&buf[..n]);
+            match body_tx.send_data(unit).await {                
+                Ok(()) => {}
+                Err(e) => return Err(Error::CustomError(format!("{:?}", e)))
+            }
+            match stream_layout.completion_tx.take() {
+                Some(completion_tx) => {
+                    match completion_tx.send(StreamCompletion::Ok) {
+                        Ok(()) => {}
+                        Err(_) => return Err(Error::CustomError("failed to send stream completion".to_owned()))
+                    }
+                }
+                None => {}
+            }            
+        }
+        ClientMsg::MessageFinished(stream_id) => {
+            let _ = stream_layouts.remove(&stream_id).ok_or(Error::CustomError("not found stream for message finish".to_owned()))?;
+        }
+        _ => {}            
+    }
+    Ok(())
+}
+*/
+
+/*
+pub enum ContentAccessType {
+    Download(Option<String>, Option<String>),
+    View(Option<String>, Option<String>)
+}
+
+pub fn response_for_content(aca_origin: Option<String>, body: hyper::Body, content_access_type: ContentAccessType) -> Result<Response<hyper::Body>, warp::http::Error> {
+    let (content_disposition, content_type) = match content_access_type {
+        ContentAccessType::Download(file_name, content_type) => (format!(r#"attachment; filename="{}""#, file_name.unwrap_or("name-missing".to_owned())), content_type.unwrap_or("".to_owned())),
+        ContentAccessType::View(file_name, content_type) => (format!(r#"inline; filename="{}""#, file_name.unwrap_or("name-missing".to_owned())), content_type.unwrap_or("".to_owned()))
+    };
+    match aca_origin {
+        Some(aca_origin) => {
+            Response::builder()                
+                .header("Access-Control-Allow-Credentials", "true")
+                .header("Access-Control-Allow-Origin", aca_origin)
+                .header("Content-Disposition", content_disposition)
+                .header("Content-Type", content_type)
+                .body(body)
+        }
+        None => {
+            Response::builder()                
+                .header("Content-Disposition", content_disposition)
+                .header("Content-Type", content_type)
+                .body(body)
+
+        }
+    }    
+}
+*/

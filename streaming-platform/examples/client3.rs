4347cbe13286943d7ab3ccbf6aad70af1ea66e27
Maximb <sprayrules@gmail.com>
2021-03-25 19:19:43 +0300

    frame

--- streaming-platform/examples/client3.rs
+++ streaming-platform/examples/client3.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 use serde_json::{json, Value, from_value};
-use streaming_platform::{client, MagicBall, sp_dto::{MsgMeta, Message, Response, resp}};
+use streaming_platform::{client, MagicBall, sp_dto::{MsgMeta, Key, Message, Response, resp}};
 
 pub async fn process_event(config: HashMap<String, String>, mut mb: MagicBall, msg: Message<Value>, _: ()) -> Result<(), Box<dyn std::error::Error>>  {
     Ok(())
@@ -11,11 +11,11 @@ pub async fn process_rpc(config: HashMap<String, String>, mut mb: MagicBall, msg
 }
 
 pub async fn startup(config: HashMap<String, String>, mut mb: MagicBall, startup_data: Option<Value>, _: ()) {
-    mb.send_event("HiEvent", json!({
+    mb.send_event(Key::simple("HiEvent"), json!({
         "data": "hello event"
     })).await;
 
-    let msg = mb.rpc::<_, Value>("HiRpc", json!({
+    let msg = mb.rpc::<_, Value>(Key::simple("HiRpc"), json!({
         "data": "hello rpc"
     })).await;
 

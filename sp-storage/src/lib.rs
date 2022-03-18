d7a84dd7c78e58934b38e09eb7e591537cda45d1
Maximb <sprayrules@gmail.com>
2022-03-18 23:02:46 +0300

    storage

--- sp-storage/src/lib.rs
+++ sp-storage/src/lib.rs
@@ -27,30 +27,10 @@ fn dog(barry: &[u8]) -> Result<[u8; 8], Error> {
     Ok(barry.try_into()?)
 }
 
-#[derive(Debug, Clone)]
-pub enum Location {
-    Regions,
-    Scopes,
-    Services { region_id: u64, scope_id: u64 },    
-    Tokens { region_id: u64, scope_id: u64, service_id: u64 },
-    Token { region_id: u64, scope_id: u64, service_id: u64, token_id: u64 }
-}
-
-impl Location {
-    fn get_scope_id(&self) -> Option<u64> {
-        match self {
-            Location::Services { region_id: _, scope_id} |
-            Location::Tokens { region_id: _, scope_id, service_id: _ } |
-            Location::Token { region_id: _, scope_id, service_id: _, token_id: _ } => Some(*scope_id),
-            _ => None
-        }
-    }
-}
-
 #[derive(Clone)]
 pub struct Dc {
     storage_path: String,
-    location: Location,
+    path: String,
     user_id: u64,
     pub id: Option<u64>,
     pub payload: Option<Value>,
@@ -59,7 +39,7 @@ pub struct Dc {
 }
 
 pub struct TxDc<'a> {    
-    location: Location,
+    path: String,
     user_id: u64,    
     db: &'a Db,
     tree: &'a TransactionalTree
@@ -104,51 +84,27 @@ impl Sc {
 }
 
 impl Dc {
-    pub fn new(location: Location, user_id: u64, storage_path: &str, id: Option<u64>, payload: Option<Value>) -> Result<Dc, Error> {
-
+    pub fn new(path: String, tree_name: &str, user_id: u64, storage_path: &str, id: Option<u64>, payload: Option<Value>) -> Result<Dc, Error> {
         let storage_path = storage_path.to_owned();
-
-        let (path, tree_name) = match location {
-            Location::Regions => (
-                storage_path.clone() + "/regions",
-                "regions".to_owned()
-            ),
-            Location::Scopes => (
-                storage_path.clone() + "/scopes",
-                "scopes".to_owned()
-            ),
-            Location::Services { region_id: _, scope_id } => (
-                storage_path.clone() + "/scope-" + &scope_id.to_string() + "/services",
-                "services".to_owned()
-            ),            
-            Location::Tokens { region_id: _, scope_id, service_id } => (
-                storage_path.clone() + "/scope-" + &scope_id.to_string() + "/svc-" + &service_id.to_string() + "-tokens",
-                "tokens".to_owned()                        
-            ),
-            Location::Token { region_id: _, scope_id, service_id, token_id } => (
-                storage_path.clone() + "/scope-" + &scope_id.to_string() + "/svc-" + &service_id.to_string() + "-token-" + &token_id.to_string(),
-                "token".to_owned()
-            )
-        };
         
-        let db = sled::open(path)?;
+        let db = sled::open(&path)?;
         let tree = db.open_tree(&tree_name)?;
 
         Ok(Dc {
             storage_path,
-            location,
+            path,
             user_id,
             id,
             payload,
             db,
             tree
         })
-    }    
+    }
     
     pub fn tx(&self, transaction_body: fn(TxDc) -> Result<(), Error>) -> Result<(), Error> {
         let res = self.tree.transaction(|tx_tree| {
             let tx_dc = TxDc {
-                location: self.location.clone(),
+                path: self.path.clone(),
                 user_id: self.user_id,                
                 db: &self.db,
                 tree: tx_tree
@@ -231,7 +187,7 @@ impl Dc {
                 self.tree.insert(id_bytes.to_vec(), &serialize(&convert_value(payload))?[..])?;
             }
             false => {
-                info!("Entity with id {} not found, location {:?}", id, self.location);
+                info!("Entity with id {} not found, path {}", id, self.path);
             }
         }
     
@@ -562,167 +518,10 @@ impl TxDc<'_> {
                 self.tree.insert(id_bytes.to_vec(), &serialize(&convert_value(payload))?[..])?;
             }
             None => {
-                info!("Entity with id {} not found, location {:?}", id, self.location);
+                info!("Entity with id {} not found, path {}", id, self.path);
             }
         }
     
         Ok(id)
     }
-}
-
-pub fn create_scope(storage_path: &str) -> Result<(u64, u64), Error> {
-    let user_id = 1;
-    
-    let region_dc = Dc::new(Location::Regions, user_id, storage_path, None, None)?;
-
-    let (region_id, mut region_payload) = match region_dc.find(|x| x["count"].as_u64().is_some() && x["count"].as_u64().unwrap() < 3)? {
-        Some((id, payload)) => (id, payload),
-        None => region_dc.create_with_payload_return(json!({ "count": 0u64 }))?,
-    };
-
-    info!("Region id {}", region_id);
-
-    region_payload["count"] = json!(match region_payload["count"].as_u64() {
-        Some(current_count) => current_count + 1,
-        None => 1
-    });    
-
-    region_dc.update(region_id, region_payload)?;
-        
-    
-    let dc = Dc::new(Location::Scopes, user_id, storage_path, None, None)?;
-    
-    let scope_id = dc.create(json!({
-        "region_id": region_id
-    }))?;
-
-    info!("Scope id {}", scope_id);
-
-    let count = dc.count(|x| x["region_id"].as_u64() == Some(region_id))?;
-
-    info!("Count is {}", count);
-
-    Ok((region_id, scope_id))
-}
-
-pub fn create_service(service_dc: &Dc, name: &str, service: &str, domain: &str) -> Result<u64, Error> {
-    Ok(service_dc.create(json!({
-        "name": name,
-		"service": service,
-		"domain": domain
-    }))?)
-}
-
-pub fn delete_service(service_dc: &Dc, name: &str, storage_path: &str) -> Result<(), Error> {
-    let (service_id, _payload) = service_dc.find(|a| a["name"].as_str() == Some(name))?.ok_or(Error::None)?;
-
-    let pattern = "svc-".to_owned() + &service_id.to_string();
-    let pattern_len = pattern.len();
-
-    let scope_path = storage_path.to_owned() + "/scope-" + &service_dc.location.get_scope_id().ok_or(Error::None)?.to_string();
-
-    info!("{}", scope_path);
-    info!("{}", pattern);
-    
-    for entry in read_dir(scope_path)? {
-        let target = entry?.path();        
-        if target.is_dir() {
-            let target_str = target.file_name().ok_or(Error::None)?;
-            
-            if target_str.len() >= pattern_len && target_str.to_str().ok_or(Error::None)?[..pattern_len] == pattern {
-                info!("Removing {}, path {:?}", target_str.to_str().ok_or(Error::None)?, target);
-
-                remove_dir_all(target)?;
-
-                info!("Ok");
-            }            
-        }
-    }
-
-    info!("Removing service from db");
-
-    service_dc.tree.remove(service_id.to_be_bytes())?.ok_or(Error::None)?;
-
-    info!("Ok");
-
-    Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use log::*;
-    use serde_json::json;
-    use crate::{
-        Location, Sc, Dc, error::Error, create_service, create_scope
-    };
-    
-    #[test]
-    fn full_test() -> Result<(), Error> {
-        let _ = env_logger::try_init();
-
-        let storage_path = "c:/src/dog";
-        let user_id = 1;
-
-        let (region_id, scope_id) = create_scope(storage_path)?;
-
-        let storage_path = "c:/src/storage";    
-
-        let service_dc = Dc::new(Location::Services { region_id, scope_id }, user_id, storage_path, None, None)?;
-
-        let service_id = create_service(&service_dc, "Dog")?;
-
-        {
-            let dc = Dc::new(Location::Tokens { region_id, scope_id, service_id }, user_id, storage_path, None, None)?;
-
-            dc.create(json!({
-                "name": "DogToken"
-            }))?;
-        }    
-
-		let sc = Sc::new(user_id, region_id, scope_id, service_id, storage_path, "Dog", "Dog", "Dog")?;
-
-        sc["DogToken"].create(json!({
-            "name": "hello"
-        }))?;
-
-        let res = sc["DogToken"].get_all()?;
-
-        info!("{:#?}", res);
-        
-        Ok(())
-    }
-
-    #[test]
-    fn test_service_delete() -> Result<(), Error> {
-        let _ = env_logger::try_init();
-
-        let storage_path = "d:/src/dog";
-        let user_id = 1;
-
-        let (region_id, scope_id) = create_scope(storage_path)?;
-
-        let storage_path = "d:/src/storage";    
-
-        let service_dc = Dc::new(Location::Services { region_id, scope_id }, user_id, storage_path, None, None)?;
-
-        let service_id = create_service(&service_dc, "Dog")?;
-
-        {
-            let dc = Dc::new(Location::Tokens { region_id, scope_id, service_id }, user_id, storage_path, None, None)?;
-
-            dc.create(json!({
-                "name": "DogToken"
-            }))?;
-        }    
-
-        let sc = Sc::new(user_id, region_id, scope_id, service_id, storage_path, "Dog", "Dog", "Dog")?;
-
-        sc["DogToken"].create(json!({
-            "name": "hello"
-        }))?;
-
-        delete_service(&service_dc, "Dog", storage_path)?;
-        
-        Ok(())
-    }    
 }
